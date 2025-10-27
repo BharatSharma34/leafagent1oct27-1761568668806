591495d5abbdce6f82b6bc864594b5e1bbf5a1dc
BharatSharma34 <bharatsharma@ducktaleit.com>
2025-10-27 18:07:50 +0530

    Create agent.py

---
+++ agent.py
@@ -0,0 +1,28 @@
+"""
+leafAgent1Oct27 - Custom Lambda Agent
+Description: test
+
+IMPORTANT: Only edit the code in the main() function below.
+The Lambda handler will be automatically appended during deployment.
+DO NOT add lambda_handler code here - it will be added automatically.
+"""
+
+def main():
+    """
+    Main function for leafAgent1Oct27
+    This function will be called by the Lambda handler.
+    
+    
+    Returns:
+    dict - JSON-serializable response
+    """
+    # Your agent logic here
+    
+    
+    return {
+        "success": True,
+        "message": "Hello from leafAgent1Oct27!",
+        "data": {}
+    }
+
+# You can add helper functions below
